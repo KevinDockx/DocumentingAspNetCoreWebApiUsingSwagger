e66ef1ae8136c6b66ad62ebe0f673d86b807f8f1
Kevin Dockx <kevin.dockx@gmail.com>
2023-12-14 17:22:31 +0100

    Starter files and finished sample

---
+++ Finished sample/Library.API/Controllers/AuthorsControllerSecondVersion.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning;
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+     [ApiVersion(2.0)]
+     [Route("api/v{version:apiVersion}/authors")]  
+    [ApiController]
+   
+    public class AuthorsControllerSecondVersion : ControllerBase
+    {
+        private readonly IAuthorRepository _authorsRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsControllerSecondVersion(
+            IAuthorRepository authorsRepository,
+            IMapper mapper)
+        {
+            _authorsRepository = authorsRepository;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Get a list of authors, V2
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            var authorsFromRepo = await _authorsRepository.GetAuthorsAsync();
+            return Ok(_mapper.Map<IEnumerable<Author>>(authorsFromRepo));
+        }
+    }
+}
+
